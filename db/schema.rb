6e5ea59ceb062df63a6a7e3b2bc6951dfb3f6484
Alan Judson <alanjudson@gmail.com>
2014-11-18 09:17:45 -0500

    added gravatar and username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117152248) do
+ActiveRecord::Schema.define(version: 20141118134842) do
 
   create_table "reviews", force: true do |t|
     t.string   "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20141117152248) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
 end
