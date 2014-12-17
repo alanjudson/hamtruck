316b655b5486934d2c1fb93ee100bef1402c5d02
Alan Judson <alanjudson@gmail.com>
2014-12-17 14:03:34 -0500

    Added page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
 
+def page_title
+	# if there is already content stored in :title, show it with 'EatSeeker'
+	if content_for?(:title)
+		content_tag(:title, "EatSeeker - #{content_for(:title)}")
+	else
+		content_tag(:title, "EatSeeker")
+	end
+end
+
+# (title) takes the @object.attr and plops it in
+# instead of writing <% content_for(:title, @user.name) %>, you can write <% title(@user.name) %>
+def title(title)
+  content_for(:title, title)
+end
 
 end
